a9f7eff70dead3ebacc19675c8d362a40f13c484
Alex Tirim <49715292+andyluak@users.noreply.github.com>
2023-12-27 01:19:25 +0200

    Refactor animation controls and variants

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,57 +6,114 @@ import {
   useAnimationControls,
 } from "framer-motion"
 
-const cardVariant = {
-  rotate: [0, -20, -10, 10, 0],
-
-  transition: {
-    duration: 0.5,
-    damping: 10,
+const cardSwipeVariants = {
+  cardVariant: {
+    rotate: [0, -20, -10, 10, 0],
+    transition: {
+      duration: 0.5,
+      damping: 10,
+    },
   },
-}
-
-const swipeVariant = {
-  width: "100%",
-  zIndex: 1,
-  opacity: [0, 0.5, 1],
-  transition: {
-    delay: 0.3,
-    duration: 1,
-    bounce: 0.5,
-    type: "spring",
-    mass: 0.2,
+  swipeVariant: {
+    width: "100%",
+    zIndex: 1,
+    opacity: [0, 0.5, 1],
+    transition: {
+      delay: 0.3,
+      duration: 1,
+      bounce: 0.5,
+      type: "spring",
+      mass: 0.2,
+    },
   },
-} as AnimationDefinition
-
-const circleVariant = {
-  zIndex: 10,
-  x: 294,
-  transition: {
-    delay: 0.2,
-    duration: 1,
-    type: "spring",
-    bounce: 0.2,
+  circleVariant: {
+    zIndex: 10,
+    x: 294,
+    transition: {
+      delay: 0.2,
+      duration: 1,
+      type: "spring",
+      bounce: 0.2,
+    },
   },
-} as AnimationDefinition
+  lineVariant: {
+    width: "110%",
+    zIndex: 10,
+    opacity: [1, 0.7, 0.5, 0],
+    x: 50,
+    transition: {
+      duration: 1,
+      type: "spring",
+      bounce: 0.2,
+    },
+  },
+  svgVariant: {
+    opacity: 1,
+    x: [0, 30, 0],
+    transition: {
+      delay: 0.4,
+      duration: 1,
+    },
+  },
+  imageVariant: {
+    x: 294,
+    opacity: [1, 0.5, 0],
+    transition: {
+      delay: 0.2,
+      duration: 1.1,
+      type: "spring",
+      bounce: 0.2,
+    },
+  },
+} as { [key: string]: AnimationDefinition }
 
-export default function Home() {
+const useCardSwipeAnimation = () => {
   const cardControls = useAnimationControls()
   const swipeControls = useAnimationControls()
   const circleControls = useAnimationControls()
+  const lineControls = useAnimationControls()
+  const svgControls = useAnimationControls()
+  const imageControls = useAnimationControls()
 
   useEffect(() => {
-    cardControls.start(cardVariant)
-    swipeControls.start(swipeVariant)
-    circleControls.start(circleVariant)
+    cardControls.start(cardSwipeVariants.cardVariant)
+    swipeControls.start(cardSwipeVariants.swipeVariant)
+    circleControls.start(cardSwipeVariants.circleVariant)
+    lineControls.start(cardSwipeVariants.lineVariant)
+    svgControls.start(cardSwipeVariants.svgVariant)
+    imageControls.start(cardSwipeVariants.imageVariant)
   }, [])
+
+  return {
+    cardControls,
+    swipeControls,
+    circleControls,
+    lineControls,
+    svgControls,
+    imageControls,
+  }
+}
+
+export default function Home() {
+  const {
+    cardControls,
+    swipeControls,
+    circleControls,
+    lineControls,
+    svgControls,
+    imageControls,
+  } = useCardSwipeAnimation()
 
   return (
     <section className="min-h-dvh flex flex-col gap-10 items-center justify-center">
       <button
         onClick={() => {
-          cardControls.start(cardVariant)
-          swipeControls.start(swipeVariant)
-          circleControls.start(circleVariant)
+          cardControls.start(cardSwipeVariants.cardVariant)
+          swipeControls.start(cardSwipeVariants.swipeVariant)
+          circleControls.start(cardSwipeVariants.circleVariant)
+          lineControls.start(cardSwipeVariants.lineVariant)
+          svgControls.start(cardSwipeVariants.svgVariant)
+          imageControls.start(cardSwipeVariants.imageVariant)
         }}
       >
         Start Animation
@@ -66,6 +123,9 @@ export default function Home() {
           cardControls.start({ rotate: 0 })
           swipeControls.start({ width: 0, opacity: 0, zIndex: -1 })
           circleControls.start({ x: 0, zIndex: -1 })
+          lineControls.start({ width: 0, opacity: 0, zIndex: -1, x: 0 })
+          svgControls.start({ opacity: 0, x: -100 })
+          imageControls.start({ x: 0, opacity: 1 })
         }}
       >
         Reset Animation
@@ -99,62 +159,34 @@ export default function Home() {
                 opacity: 0,
                 x: -100,
               }}
-              animate={{
-                opacity: 1,
-                x: [0, 30, 0],
-              }}
-              transition={{
-                delay: 0.4,
-                duration: 1,
-              }}
+              animate={svgControls}
               width="17"
               height="16"
               viewBox="0 0 17 16"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
             >
-              <motion.path
+              <path
                 d="M6.79237 12.6385L1.94511 9.27961L0.799545 10.9204L7.29743 15.423L16.9632 1.47407L15.3242 0.338343L6.79237 12.6385Z"
                 fill="white"
-                initial={{ pathLength: 0 }}
-                animate={{ pathLength: 1 }}
               />
             </motion.svg>
           </motion.div>
           <motion.div
             initial={{ x: 0, zIndex: 10 }}
-            animate={{
-              x: 294,
-              opacity: [1, 0.5, 0],
-            }}
-            transition={{
-              delay: 0.2,
-              duration: 1.1,
-              type: "spring",
-              bounce: 0.2,
-            }}
+            animate={imageControls}
             className="absolute inset-0 w-[56px] z-10 h-full rounded-full bg-gray-100"
           >
             <Image alt="" src="earth.svg" width={56} height={56} />
           </motion.div>
           <motion.div
             initial={{ width: 0, zIndex: -1, opacity: 1, x: 0 }}
-            animate={{ width: "100%", zIndex: 1, opacity: [1, 0.5, 0], x: 100 }}
-            transition={{
-              duration: 1,
-              type: "spring",
-              bounce: 0.4,
-            }}
+            animate={lineControls}
             className="absolute inset-0 w-full h-[2px] bg-pink-900 z-10 -mt-1 ml-4"
           ></motion.div>
           <motion.div
             initial={{ width: 0, zIndex: -1, opacity: 1, x: 0 }}
-            animate={{ width: "100%", zIndex: 1, opacity: [1, 0.5, 0], x: 100 }}
-            transition={{
-              duration: 1,
-              type: "spring",
-              bounce: 0.4,
-            }}
+            animate={lineControls}
             className="absolute inset-0 top-full w-full h-[2px] bg-pink-900 z-10  ml-4"
           ></motion.div>
         </div>
